c02115ec92c7ccd7e911725d9cfbe87f020158ed
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-24 18:10:42 +0500

    testing sign-in system

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Layout from "./Layout";
 import SplashScreen from "./SplashScreen";
 import avatar from "./avatar.webp";
 
-// import { initializeApp } from "firebase/app";
-// import {
-// 	getAuth,
-// 	signInWithPopup,
-// 	GoogleAuthProvider,
-// 	onAuthStateChanged,
-// 	signOut
-// } from "firebase/auth";
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyARe9LNP6X9mb0z1LFzYktjzE65GkR2zks",
-// 	authDomain: "loyality-program-e7185.firebaseapp.com",
-// 	projectId: "loyality-program-e7185",
-// 	storageBucket: "loyality-program-e7185.appspot.com",
-// 	messagingSenderId: "32610232361",
-// 	appId: "1:32610232361:web:ea6276c703a870a10bd438",
-// };
-// initializeApp(firebaseConfig);
-// const auth = getAuth();
-// const provider = new GoogleAuthProvider();
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	signInWithPopup,
+	GoogleAuthProvider,
+	// onAuthStateChanged,
+	// signOut
+} from "firebase/auth";
+const firebaseConfig = {
+	apiKey: "AIzaSyARe9LNP6X9mb0z1LFzYktjzE65GkR2zks",
+	authDomain: "loyality-program-e7185.firebaseapp.com",
+	projectId: "loyality-program-e7185",
+	storageBucket: "loyality-program-e7185.appspot.com",
+	messagingSenderId: "32610232361",
+	appId: "1:32610232361:web:ea6276c703a870a10bd438",
+};
+initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
 const restUserData = {
 	name: "User",
