83ca3930fb5395a800be551830305e424911c85a
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-24 18:06:22 +0500

    testing sign-in system

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ export default function App({ children }) {
 			}
 		};
 		getCameras();
-	}, [renderState]);
+	}, [renderState, splash]);
 
 	// useEffect(() => {
 	// onAuthStateChanged(auth, (result) => {
