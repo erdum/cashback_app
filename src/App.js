9041205c3bfdf205c2614c18d9670459b787790f
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-25 16:58:26 +0500

    isolating camera module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 // import Layout from "./Layout";
 import SplashScreen from "./SplashScreen";
