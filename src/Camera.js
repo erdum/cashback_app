9f706a0fa996308a57ff1656d844d8690ef173da
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-26 19:26:57 +0500

    isolating camera module

--- src/Camera.js
+++ src/Camera.js
@@ -7,27 +7,37 @@ const videoConstraints = {
 };
 
 const Camera = () => {
-	// const [image, setImage] = React.useState(null);
+	const [image, setImage] = React.useState(null);
 	const webcamRef = React.useRef(null);
 
 	const capture = React.useCallback(() => {
 		const imageSrc = webcamRef.current.getScreenshot();
-		alert(imageSrc);
-		// setImage(imageSrc);
+		setImage(imageSrc);
 	}, [webcamRef]);
 
 	return (
 		<div className="camera-wrapper">
-			<Webcam
-				className="camera"
-				style={{ width: "75vw", height: "70vh" }}
-				audio={false}
-				ref={webcamRef}
-				screenshotFormat="image/jpeg"
-				videoConstraints={videoConstraints}
-			/>
-				<button onClick={capture}>Capture Reciept</button>
-				<button>Change Camera</button>
+			{image === null ? (
+				<Webcam
+					className="camera"
+					style={{ width: "75vw", height: "75vh" }}
+					audio={false}
+					ref={webcamRef}
+					screenshotFormat="image/jpeg"
+					videoConstraints={videoConstraints}
+				/>
+			) : (
+				<img
+					className="camera"
+					src={image}
+					alt=""
+					style={{ width: "75vw", height: "75vh" }}
+				/>
+			)}
+			<button onClick={capture}>
+				{image === null ? "Capture Reciept" : "Capture Agian"}
+			</button>
+			{image === null ? <button>Change Camera</button> : null}
 		</div>
 	);
 };
