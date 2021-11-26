1fd559a53c9f7504b7d626383d5242ff7ba3abc7
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-26 20:14:22 +0500

    isolating camera module

--- src/Camera.js
+++ src/Camera.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import "./camera.css";
 
