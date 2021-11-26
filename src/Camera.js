c2548f332e1250f86deb9d0f6bf3e8076dc3b9b0
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-26 11:15:56 +0500

    isolating camera module

--- src/Camera.js
+++ src/Camera.js
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 const Camera = forwardRef((props, ref) => {
 	return(
 		<div className="video-wrapper">
-			<video className="camera" width="100%" ref={ref}></video>
+			<video className="camera" width="100%" style={{ height: "100vh" }} ref={ref}></video>
 		</div>
 	);
 });
