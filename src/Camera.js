af6e5d079ae8536e1ceceff12f7a6c383079e303
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-26 18:23:33 +0500

    isolating camera module

--- src/Camera.js
+++ src/Camera.js
@@ -16,7 +16,7 @@ const Camera = () => {
 	}, [webcamRef]);
 
 	return (
-		<>
+		<div className="camera-wrapper">
 			<Webcam
 				className="camera"
 				style={{ width: "75vw", height: "75vh" }}
@@ -29,7 +29,7 @@ const Camera = () => {
 				{/*<button>Change Camera</button>*/}
 				<button>Capture Reciept</button>
 			</Webcam>
-		</>
+		</div>
 	);
 };
 
