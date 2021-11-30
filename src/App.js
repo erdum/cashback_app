2f271481a301adea091d66b603d86bcd7f883c50
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-30 20:23:38 +0500

    using ocr api

--- src/App.js
+++ src/App.js
@@ -121,7 +121,8 @@ export default function App({ children }) {
 		const apiBase = "https://api.cloudmersive.com/ocr/receipts/photo/to/csv";
 
 		const formdata = new FormData();
-		formdata.append("imageFile", image, "file");
+		// formdata.append("imageFile", image);
+		formdata.append("imageFile", "https://www.inogic.com/blog/wp-content/uploads/2020/09/Receipt-Processor-AI-Builder-in-Canvas-App-9.png");
 
 		const requestOptions = {
 			method: "POST",
