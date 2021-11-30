3454ecad9f7c1dfd755f512d720eb2509feb6363
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-11-30 20:17:14 +0500

    using ocr api

--- src/App.js
+++ src/App.js
@@ -117,16 +117,17 @@ export default function App({ children }) {
 	};
 
 	const scanReceipt = async (image) => {
-		const apiBase = "https://api.ocr.space/parse/image";
+		// const apiBase = "https://api.ocr.space/parse/image";
+		const apiBase = "https://api.cloudmersive.com/ocr/receipts/photo/to/csv";
 
 		const formdata = new FormData();
-		formdata.append("base64Image", image);
-		formdata.append("filetype", "PNG");
+		formdata.append("imageFile", image, "file");
 
 		const requestOptions = {
 			method: "POST",
 			headers: {
-				"apikey": "5b8c7cfbc188957",
+				// "Apikey": "5b8c7cfbc188957",
+				"Apikey": "628139e5-482e-46c9-8734-7293d6e2a500",
 			},
 			body: formdata,
 			redirect: "follow",
