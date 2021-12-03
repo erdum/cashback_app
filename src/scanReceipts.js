2eb542edabca80fd2be47a93e6dd103ad6cd4b2f
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-03 20:44:15 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -31,15 +31,33 @@ const scanReceipt = async (image) => {
 
 		// xhr.send(data);
 
-		let data = "urls=http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg";
+		// const options = {
+		// 	url: "https://app.nanonets.com/api/v2/OCR/Model/{{model_id}}/LabelUrls/",
+		// 	body: querystring.stringify(form_data),
+		// 	headers: {
+		// 		Authorization:
+		// 			"Basic " +
+		// 			Buffer.from("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd" + ":").toString(
+		// 				"base64"
+		// 			),
+		// 	},
+		// };
 
-		const res = await fetch("https://app.nanonets.com/api/v2/OCR/Model/4616eef1-9ee9-4a4b-beba-88eba6135f89/LabelUrls/", {
+		const form_data = {
+			urls: [
+				"https://nanonets.s3-us-west-2.amazonaws.com/test-images/test1.jpg",
+			],
+		};
+
+		const url = "https://app.nanonets.com/api/v2/OCR/Model/{{b44f74de-9587-434b-b706-8d6c1f2b68bd}}/LabelUrls/";
+
+		const res = await fetch(url, {
 			"method": "post",
-			"Content-Type": "application/x-www-form-urlencoded",
 			headers: {
-				"authorization": "Basic " + btoa("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd:"),
-			},
-			body: data
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Authorization": "Basic " + btoa("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd:"),
+			}
+			body: JSON.stringify(form_data);
 		});
 
 		console.log(res);
