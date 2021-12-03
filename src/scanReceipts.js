a8d8ab50eb9522da33f271cb63198fe37d3e0c9e
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-03 18:30:37 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -17,20 +17,20 @@ const scanReceipt = async (image, setOcrLoader) => {
 		let API_KEY = "1dce7281a3649b4d9bc742926ab3d3a4";
 		let CLIENT_ID = "vrfOeWTZOsxe66HHw6iVtD2NMeroxmI8mWOoL59";
 		let ENVIRONMENT_URL = "https://api.veryfi.com/";
+		let	url = "https://" + ENVIRONMENT_URL + "/api/v7/partner/documents/",
 
 		let options = {
 			method: "POST",
-			uri: "https://" + ENVIRONMENT_URL + "/api/v7/partner/documents/",
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
 				"CLIENT-ID": CLIENT_ID,
 				AUTHORIZATION: `apikey ${USER_NAME}:${API_KEY}`,
 			},
-			json: jsonData,
+			body: JSON.stringify(jsonData),
 		};
 
-		const res = await fetch(options);
+		const res = await fetch(url=url, options);
 		const data = await res.json();
 		console.log(data);
 	} catch (err) {
