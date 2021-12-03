545f75b84be750d17de6e019fa8df79c5433d603
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-03 20:57:02 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -2,54 +2,9 @@
 
 const scanReceipt = async (image) => {
 	try {
-		// const {
-		// 	data: { text },
-		// } = await Tesseract.recognize(image, "eng");
-		// alert(text);
-
-		// var data = new FormData();
-		// data.append("file", image); // This is file object
-
 		// http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg
-		// var xhr = new XMLHttpRequest();
 
-		// xhr.addEventListener("readystatechange", function () {
-		// 	if (this.readyState === this.DONE) {
-		// 		console.log(this.responseText);
-		// 	}
-		// });
-
-		// xhr.open(
-		// 	"POST",
-		// 	"https://app.nanonets.com/api/v2/OCR/Model/4616eef1-9ee9-4a4b-beba-88eba6135f89/LabelUrls/"
-		// );
-		// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		// xhr.setRequestHeader(
-		// 	"authorization",
-		// 	"Basic " + btoa("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd:")
-		// );
-
-		// xhr.send(data);
-
-		// const options = {
-		// 	url: "https://app.nanonets.com/api/v2/OCR/Model/{{model_id}}/LabelUrls/",
-		// 	body: querystring.stringify(form_data),
-		// 	headers: {
-		// 		Authorization:
-		// 			"Basic " +
-		// 			Buffer.from("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd" + ":").toString(
-		// 				"base64"
-		// 			),
-		// 	},
-		// };
-
-		const form_data = {
-			urls: [
-				"https://nanonets.s3-us-west-2.amazonaws.com/test-images/test1.jpg",
-			],
-		};
-
-		const url = "https://app.nanonets.com/api/v2/OCR/Model/{{b44f74de-9587-434b-b706-8d6c1f2b68bd}}/LabelUrls/";
+		const url = "https://app.nanonets.com/api/v2/OCR/Model/4616eef1-9ee9-4a4b-beba-88eba6135f89/LabelUrls/";
 
 		const res = await fetch(url, {
 			"method": "post",
@@ -57,7 +12,7 @@ const scanReceipt = async (image) => {
 				"Content-Type": "application/x-www-form-urlencoded",
 				"Authorization": "Basic " + btoa("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd:"),
 			},
-			body: "urls=" + form_data.urls[0]
+			body: "urls=http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg"
 		});
 
 		console.log(res);
