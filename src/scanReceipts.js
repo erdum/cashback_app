0faac7b54cb59bcf2e549d44fcd10916d69fe264
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-03 20:02:08 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -10,24 +10,31 @@ const scanReceipt = async (image) => {
 		// var data = new FormData();
 		// data.append("file", image); // This is file object
 
+		var data = "urls=http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg";
+// http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg
 		var xhr = new XMLHttpRequest();
 
 		xhr.addEventListener("readystatechange", function () {
 			if (this.readyState === this.DONE) {
-				alert(this.responseText);
+				console.log(this.responseText);
 			}
 		});
 
 		xhr.open(
 			"POST",
-			"https://app.nanonets.com/api/v2/OCR/Model/4616eef1-9ee9-4a4b-beba-88eba6135f89/LabelFile/"
+			"https://app.nanonets.com/api/v2/OCR/Model/4616eef1-9ee9-4a4b-beba-88eba6135f89/LabelUrls/"
 		);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.setRequestHeader(
 			"authorization",
 			"Basic " + btoa("B3PwpQahecZhnnzG6ciTD-MxZJyiIlyd:")
 		);
 
-		xhr.send("urls=http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg");
+		xhr.send(data);
+
+		// xhr.send(
+		// 	"urls=http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg"
+		// );
 	} catch (err) {
 		alert(err);
 	}
