7f1cd879256bfab96170a8df59a70ab92aa6120a
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-05 12:33:04 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -1,24 +1,24 @@
 const scanReceipt = async (image) => {
-	try {
-		const worker = createWorker({
-			logger: (m) => console.log(m),
-		});
+	// try {
+	// 	const worker = createWorker({
+	// 		logger: (m) => console.log(m),
+	// 	});
 
-		(async () => {
-			await worker.load();
-			await worker.loadLanguage("eng");
-			await worker.initialize("eng");
-			const {
-				data: { text },
-			} = await worker.recognize(
-				"https://tesseract.projectnaptha.com/img/eng_bw.png"
-			);
-			console.log(text);
-			await worker.terminate();
-		})();
-	} catch (err) {
-		return err;
-	}
+	// 	(async () => {
+	// 		await worker.load();
+	// 		await worker.loadLanguage("eng");
+	// 		await worker.initialize("eng");
+	// 		const {
+	// 			data: { text },
+	// 		} = await worker.recognize(
+	// 			"https://tesseract.projectnaptha.com/img/eng_bw.png"
+	// 		);
+	// 		console.log(text);
+	// 		await worker.terminate();
+	// 	})();
+	// } catch (err) {
+	// 	return err;
+	// }
 };
 
 export default scanReceipt;
