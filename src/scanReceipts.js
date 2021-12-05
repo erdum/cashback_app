3905f43880c41be194d5c53d4f9493917c392b6a
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-05 11:38:34 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -1,17 +1,10 @@
-import Tesseract from "tesseract.js";
-
 const scanReceipt = async (image) => {
 	try {
 		// const exampleImage = "http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg";
 
-		Tesseract.recognize(
-			"https://tesseract.projectnaptha.com/img/eng_bw.png",
-			"eng",
-			{ logger: (m) => console.log(m) }
-		).then(({ data: { text } }) => {
-			console.log(text);
+		Tesseract.recognize(image, function (result) {
+			console.log(result);
 		});
-
 	} catch (err) {
 		return err;
 	}
