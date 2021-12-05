93a9849bfe8118eb03dba5e72a009ca1e79a2e44
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-05 10:53:39 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -1,21 +1,16 @@
-import { createWorker } from "tesseract.js";
+import Tesseract from "tesseract.js";
 
 const scanReceipt = async (image) => {
 	try {
 		// const exampleImage = "http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg";
 
-		let img = await fetch(image);
-		img = img.blob();
-
-		let worker = createWorker({
-			logger: m => console.log(m)
+		Tesseract.recognize(
+			"https://tesseract.projectnaptha.com/img/eng_bw.png",
+			"eng",
+			{ logger: (m) => console.log(m) }
+		).then(({ data: { text } }) => {
+			console.log(text);
 		});
-		await worker.load();
-		await worker.loadlanguage("eng");
-		await worker.initialize("eng");
-		const { data: { text } } = await worker.recognize(img);
-		await worker.terminate();
-		console.log(text);
 
 	} catch (err) {
 		return err;
