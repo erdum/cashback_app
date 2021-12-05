8b9d005671263248dd1cb9a3b4e9b6e84a0deac3
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-05 11:42:33 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -1,3 +1,5 @@
+import Tesseract from "tesseract.js";
+
 const scanReceipt = async (image) => {
 	try {
 		// const exampleImage = "http://www.printablesample.com/wp-content/uploads/2017/03/Short-Grocery-Receipt-Format-3.jpg";
