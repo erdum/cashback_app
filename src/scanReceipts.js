621ad574a8b02086b3f66a454aeaa20b5e9e2d88
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-06 17:32:06 +0500

    finalizing the OCR system

--- src/scanReceipts.js
+++ src/scanReceipts.js
@@ -1,16 +1,27 @@
 import Tesseract from "tesseract.js";
 import test from "./rec.jpg";
 
+const blobToBase64 = (blob) => {
+	const reader = new FileReader();
+	reader.readAsDataURL(blob);
+	return new Promise((resolve) => {
+		reader.onloadend = () => {
+			resolve(reader.result);
+		};
+	});
+};
+
 const scanReceipt = async (image, progress) => {
 	try {
 		let newImg = await fetch(image, { mode: "cors" });
 		newImg = await newImg.blob();
-		newImg = URL.createObjectURL(newImg);
+		const img64 = await blobToBase64(newImg);
 		console.log(newImg);
 		console.log(test);
+		console.log(img64);
 		const {
 			data: { text },
-		} = await Tesseract.recognize(newImg, "eng", { logger: progress });
+		} = await Tesseract.recognize(img64, "eng", { logger: progress });
 		return text;
 	} catch (err) {
 		return err;
