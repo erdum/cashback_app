bac5f1c55d38b51f26db9edc17a801ef28c5c570
Syed M Erdum Adnan <erdumadnan@gmail.com>
2021-12-07 23:58:23 +0500

    finalizing the OCR system

--- src/Layout.js
+++ src/Layout.js
@@ -68,7 +68,7 @@ const Layout = forwardRef((props, ref) => {
 					</>
 				) : props.data}*/}
 				<h2>Processing Image...</h2>
-				<LinearProgress variant={props.scanProcessStart} value={props.scanProcessValue} />
+				<LinearProgress variant={props.scanProcessStart} />
 			</div>
 		</div>
 	);
